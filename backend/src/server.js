50f46e4898552d328e1d99c70c87215463dc71cb
alamincse97 <alamin.cse97@gmail.com>
2025-06-21 11:23:52 +0600

    [ADD] make a getRecommendedUsers

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,10 +1,12 @@
 import express from 'express';
 import dotenv from 'dotenv';
-import authRoutes from './routes/auth.route.js';
-import { connectDB } from './lib/db.js';
 import cookieParser from 'cookie-parser';
-
 dotenv.config();
+
+import authRoutes from './routes/auth.route.js';
+import userRoutes from './routes/user.route.js';
+
+import { connectDB } from './lib/db.js';
 
 const app = express();
 const PORT = process.env.PORT || 5000;
@@ -13,6 +15,7 @@ app.use(express.json());
 app.use(cookieParser());
 
 app.use("/api/auth", authRoutes);
+app.use("/app/users", userRoutes);
 
 app.listen (PORT, () => {
     console.log(`Server is running on port ${PORT}`);
