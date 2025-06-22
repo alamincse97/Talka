8c62a39cebd32e9643877fac7ad5b558289a0e05
alamincse97 <alamin.cse97@gmail.com>
2025-06-22 11:20:32 +0600

    [ADD] making a server part

--- backend/src/server.js
+++ backend/src/server.js
@@ -7,6 +7,7 @@ import path from 'path';
 
 import authRoutes from './routes/auth.route.js';
 import userRoutes from './routes/user.route.js';
+import chatRoutes from "./routes/chat.route.js";
 
 import { connectDB } from './lib/db.js';
 
@@ -20,6 +21,7 @@ app.use(cookieParser());
 
 app.use("/api/auth", authRoutes);
 app.use("/app/users", userRoutes);
+app.use("/api/chat", chatRoutes);
 
 app.listen (PORT, () => {
     console.log(`Server is running on port ${PORT}`);
