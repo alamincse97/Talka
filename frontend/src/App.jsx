7dcea524dcbd07dcf9ab57d75ebc4ece555a367c
alamincse97 <alamin.cse97@gmail.com>
2025-06-23 15:36:46 +0600

    [ADD] frontend setup

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,4 +1,15 @@
 const App = () => {
-    return <div className="bg-red-500 h-screen text-5xl">App</div>
+  return (
+    <div className="bg-red-500 h-screen text-5xl" data-theme="forest">
+      <button className="btn">Button</button>
+      <button className="btn btn-neutral">Neutral</button>
+      <button className="btn btn-primary">Primary</button>
+      <button className="btn btn-secondary">Secondary</button>
+      <button className="btn btn-accent">Accent</button>
+      <button className="btn btn-ghost">Ghost</button>
+      <button className="btn btn-link">Link</button>
+    </div>
+  );
 };
+
 export default App;
