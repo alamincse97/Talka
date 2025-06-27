94ed859527a5c1bba7a5a6b8e03a89ee9d27f011
alamincse97 <alamin.cse97@gmail.com>
2025-06-27 18:24:13 +0600

    [ADD] middleware cookies

--- backend/src/middleware/auth.middleware.js
+++ backend/src/middleware/auth.middleware.js
@@ -4,19 +4,19 @@ import User from "../models/User.js";
 
 export const protectRoute = async (req, res, next) =>{
     try{
-        const token = req.cookes?.jwt;
+        const token = req.cookies?.jwt;
 
         if(!token){
             return res.status(401).json({ message: "Unauthorized - No token provided" });
         }
 
-        const decode = jwt.verify(token, process.env.JWT_SECRET_KEY);
+        const decoded = jwt.verify(token, process.env.JWT_SECRET_KEY);
 
-        if(!decode){
+        if(!decoded){
             return res.status(401).json({ message: "Unauthorized - Invalid token" });
         }
 
-        const user = await User.findById(decode.userId).select("-password");
+        const user = await User.findById(decoded.userId).select("-password");
 
         if(!user){
             return res.status(401).json({ message: "Unauthorized - User not found" });
